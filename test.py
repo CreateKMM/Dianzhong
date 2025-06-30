50463d2101cf99a0a029ecfd4c727ce0f946bfa7
CreateKMM <1158716173@qq.com>
2025-06-30 13:52:44 +0800

    debug

---
+++ test.py
@@ -0,0 +1,8 @@
+import os
+
+def get_file_list(folder_path):
+    file_list = []
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            file_list.append(os.path.join(root, file))
+    return file_list
